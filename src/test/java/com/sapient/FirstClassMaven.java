9d10cb1819c75b83b5e99a169b4328de0df1c20c
Harshini S m <harsm@publicisgroupe.net>
2022-11-29 22:23:02 +0530

    added

---
+++ src/test/java/com/sapient/FirstClassMaven.java
@@ -0,0 +1,12 @@
+package com.sapient;
+
+public class FirstClassMaven {
+
+	public static void main(String[] args) {
+		String firstname=" Harshini";
+		String lastname=" SM";
+		System.out.println("Hello "+firstname.concat(lastname));
+
+	}
+
+}
